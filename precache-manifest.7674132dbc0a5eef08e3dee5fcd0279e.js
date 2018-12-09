81358884819922fda77c297ec04ecc27225d89b6
bryan-lee-git <bryanvinyl@gmail.com>
2018-12-09 15:39:25 -0700

    Updates

---
+++ precache-manifest.7674132dbc0a5eef08e3dee5fcd0279e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bf1df017e5eca48a17e",
+    "url": "/clicky-game/static/css/main.fc6f3105.chunk.css"
+  },
+  {
+    "revision": "9bf1df017e5eca48a17e",
+    "url": "/clicky-game/static/js/main.9bf1df01.chunk.js"
+  },
+  {
+    "revision": "d649579a62c8d728c4be",
+    "url": "/clicky-game/static/js/1.d649579a.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "0b6562c4992749c9238902808d78cf26",
+    "url": "/clicky-game/index.html"
+  }
+];
